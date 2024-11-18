53e09a0fea65b8789ce8f5d75f74a35c26ceafc3
alihuzaifa <ha4587323@gmail.com>
2024-11-19 01:27:30 +0500

    dashboard layout with css hackathon website practice project and javascript intro has pushed or covered in class

---
+++ Javascript/1) Intro Variables & Alert/app.js
@@ -0,0 +1,27 @@
+// alert('Hello World');
+
+
+// alert(10 + 10);
+
+
+
+// Datatypes 
+
+// String => all characters except numbers  '', "", ``
+// Numbers => 1234567890 
+
+
+// alert('Hello');
+
+// alert('10');
+
+
+// var quotation = 'A quick brown fox jumps over the lazy dog';
+
+// var number1 = 10;
+// var number2 = 130;
+
+// alert(number1 + number2);
+
+
+alert("Hello \n World")
---
+++ Javascript/javascript-exercises/README.md
@@ -0,0 +1,4 @@
+# JavaScript Exercises for "A Smarterway To Learn JavaScript Book"
+
+This repository contains chapter-wise JavaScript exercises from the book "A Smarterway To Learn JavaScript".
+
