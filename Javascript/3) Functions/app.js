588b86558fe7aaf18fe39af0ff58fd70a0592950
alihuzaifa <ha4587323@gmail.com>
2024-11-26 02:16:32 +0500

    Function has been covered in class

---
+++ Javascript/3) Functions/app.js
@@ -0,0 +1,92 @@
+
+// greetMessage();
+
+
+
+// function greetMessage() {
+//     console.log("Hello How are You");
+// }
+// greetMessage();
+// greetMessage();
+// greetMessage();
+// greetMessage();
+// greetMessage();
+// greetMessage();
+// greetMessage();
+// greetMessage();
+// greetMessage();
+// greetMessage();
+
+
+// console.log(greetMessage);
+
+// var greetMessage = "Hello How are You";
+
+
+
+// function sum() {
+// var number1 = +prompt("Enter Number 1");
+// var number2 = +prompt("Enter Number 2");
+// }
+// console.log(number1 + number2 + 500);
+
+// sum();
+
+// function sum() {
+//     var number1 = +prompt("Enter Number 1");
+//     var number2 = +prompt("Enter Number 2");
+//     var output = number1 + number2
+//     return output;
+
+// }
+
+// var result = sum();
+// console.log(result);
+
+
+
+
+// function sum(number1, number2) { // Parameters
+//     return number1 + number2
+// }
+
+// var result = sum(100, 200); // Arguements
+// var result2 = sum(3638, 443);
+// var result3 = sum(213, 4321);
+// var result4 = sum(22, 3937973);
+
+
+// console.log(result);
+// console.log(result2);
+// console.log(result3);
+// console.log(result4);
+
+
+
+
+
+
+// function mergeName(firstName, lastName) {
+//     return firstName + " " + lastName
+// }
+
+// var result = mergeName("Ali", "Huzaifa");
+// console.log(result);
+
+
+// function square(number) {
+//     return number * number
+// }
+// var userNumber = +prompt("Enter Your Square Number")
+// var result = square(userNumber);
+// console.log(result);
+
+
+
+
+function calculateArea(length, width) {
+    return length * width
+}
+
+var result = calculateArea(10, 20)
+console.log(result);
