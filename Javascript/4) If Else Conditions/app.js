a4c01b1185915abac1ce54124f693afb33f9a20f
alihuzaifa <ha4587323@gmail.com>
2024-12-02 18:35:30 +0500

    if else condition has covered

---
+++ Javascript/4) If Else Conditions/app.js
@@ -0,0 +1,127 @@
+// Bodmas
+// var totalCost = 1 + 3 * 4;
+// var totalCost = 1 + (3 * 4 + (2 * 4));
+// 1 3 * 4 + 8
+// 1 12+8
+
+// var totalCost = (2 * 4) * (4 + 2);
+
+// console.log(totalCost)
+
+
+
+// var number = 0;
+// number = number - 1;
+
+// Increment
+// number++;
+// number++;
+// number++;
+
+// Decrement
+// number--
+// number--
+// number--
+
+// Post variable call second time then update variable++ || variable--
+
+// Pre variable call first time then update ++variable || --variable;
+
+
+// console.log(--number);
+// console.log(number);
+
+// var firstName = "Asad ";
+// var lastName = "Amir";
+// console.log(firstName + "\n" + lastName);
+
+// var number = 1;
+// ===
+
+// Comparison Operators
+
+// var userAge = +prompt("Enter Your Age?")
+// === > < >= <= !==
+
+// if (userAge <= 18) {
+//     alert("You are teenager");
+// }
+
+// var favSubject = prompt("Enter Your Favourite Subject Name?");
+// if (favSubject !== "Math") {
+//     alert("Good");
+// }
+
+
+// var itemToBuy = "Roti";
+// if (itemToBuy === "Paratha") {
+//     alert("Buy 5")
+// } else {
+//     alert("Please Buy Naan");
+// }
+
+
+
+// 
+
+// var cityName = prompt("Enter Your City Name");
+// if (cityName === "Karachi") {
+//     console.log("Welcome to city of lights")
+// } else {
+//     console.log("Good City")
+// }
+
+
+// var userNumber = +prompt("Enter Your Number");
+// if (userNumber % 2 === 0) {
+//     console.log("Even Number");
+// } else {
+//     console.log("Odd Number");
+
+// }
+
+
+// var userAge = +prompt("Enter Your Age");
+// if (userAge >= 18) {
+//     console.log("Eligible");
+
+// } else {
+//     console.log("Not Eligible");
+
+// }
+
+
+// var year = +prompt("Enter Your Year");
+// if (year % 4 === 0) {
+//     console.log("Leap Year");
+
+// } else {
+//     console.log("Not Leap Year");
+
+// }
+
+
+var character = prompt("Enter Your Character");
+if (character === "a") {
+    console.log("Vowels");
+
+}
+if (character === "e") {
+    console.log("Vowels");
+
+}
+if (character === "i") {
+    console.log("Vowels");
+
+}
+if (character === "o") {
+    console.log("Vowels");
+
+}
+if (character === "u") {
+    console.log("Vowels");
+
+}
+else {
+    console.log("Not Vowels");
+}
