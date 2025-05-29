578eab316af60e23a66134a548dc863721b3f020
alihuzaifa <ha4587323@gmail.com>
2025-05-29 13:09:20 +0500

    Inheritance Static Method Access Modifier has covered

---
+++ Typescript/4) Classes , Inheritance , Access Modifier , Static Method/dist/app.js
@@ -0,0 +1,53 @@
+"use strict";
+// Classes
+// class User {
+//   userName: string;
+//   userAge: number;
+//   constructor(name: string, age: number) {
+//     this.userName = name;
+//     this.userAge = age;
+//   }
+// }
+// // const user1 = new User("Ali Huzaifa", 22);
+// // console.log(user1);
+// class Student extends User {
+//   userQualification: string;
+//   constructor(name: string, age: number, qualification: string) {
+//     super(name, age);
+//     this.userQualification = qualification;
+//   }
+// }
+// const student1 = new Student("Aslam", 22, "Matric");
+// console.log(student1);
+// class PrivateCar {
+//   private speed: number;
+//   constructor(param: number) {
+//     this.speed = param;
+//   }
+//   get getSpeed() {
+//     return this.speed;
+//   }
+//   set setSpeed(value: number) {
+//     this.speed = value;
+//   }
+// }
+// const car1 = new PrivateCar(80);
+// car1.setSpeed = 1000000
+// console.log(car1);
+// var date = new Date().getTime()
+// console.log(Date.now());
+// class Calculator {
+//   constructor() {}
+//   static add(number1: number, number2: number) {
+//     return number1 + number2;
+//   }
+//   static sub(number1: number, number2: number) {
+//     return number1 - number2;
+//   }
+//   static mul(number1: number, number2: number) {
+//     return number1 * number2;
+//   }
+// }
+// const result = new Calculator();
+// // console.log(result.add(10, 10));
+// console.log(Calculator.add(10, 20));
